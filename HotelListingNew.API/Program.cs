7f66af851dccdcb10b8109025e818812ddb96d00
Omer elhyani <omer37374@gmail.com>
2023-02-21 15:23:56 +0200

    Added Entity Framework and Created Database

--- HotelListingNew.API/Program.cs
+++ HotelListingNew.API/Program.cs
@@ -1,8 +1,16 @@
+using HotelListingNew.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+//using appsettings.json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options => { 
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
